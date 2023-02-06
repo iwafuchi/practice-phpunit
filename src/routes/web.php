0e351175d25219c37779cf1594d45451278e6aae
iwafuchi <rta375@gmail.com>
2023-02-06 23:55:07 +0900

    バリデーションルールでレコードの値存在チェックをかけてエラーを返す

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\User\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::controller(UserController::class)->prefix('user')->group(function () {
+    Route::get('/', 'index')->name('user.index');
+    Route::get('/create', 'create')->name('user.create');
+    Route::post('/create', 'store')->name('user.store');
 });
